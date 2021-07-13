53b0727c47d27d018dc66e5fa311a390fa15422e
Hayden Carroll <haydencarroll77@gmail.com>
2021-07-13 10:12:09 -0700

    added some media query rulings

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -92,18 +92,20 @@ export default class Home extends Component {
                                 <img src={ProfileImg} alt="Portrait of me"/>
                             </div>
                             <div className="about-text">
-                                My name is Hayden Carroll and I am a recent Computer Science graduate
-                                from North Idaho College. I have been very passionate about Computer
-                                Science my whole life, specifically software development, Through the years,
-                                I have been in charge of and a part of many different extracurricular Computer
-                                Science clubs such as leader of the North Idaho College Robotics Team, Website
-                                lead for Project DaVinci, and team member of the North Idaho College ICPC
-                                programming competition for 2021. Currently, I am a coding mentor teaching
-                                middle school through high school students the fundamentals of web develement
-                                at North Idaho College. Starting in the Fall of 2021, I will be attending the
-                                University of Idaho to finish my Bachelor's degree in Computer Science in the
-                                Fall of 2022. I hope to land a job working in software development right after
-                                college!
+                                <p>
+                                    My name is Hayden Carroll and I am a recent Computer Science graduate
+                                    from North Idaho College. I have been very passionate about Computer
+                                    Science my whole life, specifically software development, Through the years,
+                                    I have been in charge of and a part of many different extracurricular Computer
+                                    Science clubs such as leader of the North Idaho College Robotics Team, Website
+                                    lead for Project DaVinci, and team member of the North Idaho College ICPC
+                                    programming competition for 2021. Currently, I am a coding mentor teaching
+                                    middle school through high school students the fundamentals of web develement
+                                    at North Idaho College. Starting in the Fall of 2021, I will be attending the
+                                    University of Idaho to finish my Bachelor's degree in Computer Science in the
+                                    Fall of 2022. I hope to land a job working in software development right after
+                                    college!
+                                </p>
                             </div>
                         </div>
 
