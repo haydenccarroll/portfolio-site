6a07dd5d4fdf892eb415524bd733130f55302545
Hayden Carroll <haydencarroll77@gmail.com>
2021-07-14 10:01:26 -0700

    pushed to github pages

--- src/components/app.js
+++ src/components/app.js
@@ -87,8 +87,7 @@ export default class App extends Component {
     render() {
         return (
             <div className='container'>
-
-                <Router basename={`${process.env.PUBLIC_URL}`}>
+                <Router basename="/portfolio-site/">
                     <div>
                         <NavigationContainer 
                             loggedInStatus={this.state.loggedInStatus}
