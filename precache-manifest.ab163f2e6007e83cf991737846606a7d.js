ae2656b7946ed7754cdf72236062a15a5d2d396c
Hayden Carroll <haydencarroll77@gmail.com>
2022-07-10 12:37:03 -0700

    Updates

---
+++ precache-manifest.ab163f2e6007e83cf991737846606a7d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535b45d88d933a327bf83c428751b3ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d043e1bb6f70a453ef2f",
+    "url": "/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "65ea90258c80eee58eb3",
+    "url": "/static/css/main.f7327c62.chunk.css"
+  },
+  {
+    "revision": "d043e1bb6f70a453ef2f",
+    "url": "/static/js/2.624041b0.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/static/js/2.624041b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65ea90258c80eee58eb3",
+    "url": "/static/js/main.ca88e4a7.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  },
+  {
+    "revision": "dc874333a14b7c3d32bf961752955bfe",
+    "url": "/static/media/artemis.dc874333.png"
+  },
+  {
+    "revision": "f8b2d662efaf8e87e910e3716707cdab",
+    "url": "/static/media/cubesat.f8b2d662.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "a9703379a63b395112e240adf1f73ed1",
+    "url": "/static/media/out-of-the-dark.a9703379.png"
+  },
+  {
+    "revision": "2aec5a9098348a6e983703ca0f5b728a",
+    "url": "/static/media/portrait.2aec5a90.jpg"
+  }
+]);
